fb06c9109d026d3a6a6f3c41080b0a65604d2d72
Antoine Guglielmi <contact.antoine.guglielmi@gmail.com>
2025-07-26 20:54:40 +0200

    PXN-4 - Add Polariis trigger button to Polariis component

--- polariis/components/polariis-provider.tsx
+++ polariis/components/polariis-provider.tsx
@@ -5,9 +5,19 @@ type PolariisProviderProps = {
 }
 
 export default function PolariisProvider({ children }: PolariisProviderProps) {
+  const triggerPolariis = async () => {
+    console.log('Polariis triggered')
+  }
+
   return (
     <>
-      PolariisProvider
+      <button
+        className="flex items-center justify-center h-12 w-12 rounded-full bg-[#21242A] text-[#DEDBD5] fixed top-4 -left-[9999px] focus:left-4"
+        tabIndex={0}
+        onClick={triggerPolariis}
+      >
+        ii
+      </button>
       {children}
     </>
   )
