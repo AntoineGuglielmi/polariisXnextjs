85ddbdbe8d09e13ee8498057bf209561a2d0dfbd
Antoine Guglielmi <contact.antoine.guglielmi@gmail.com>
2025-08-10 17:24:30 +0200

    30 - implement TTS service then read the audio

--- polariis/actions/ActionGetDescription.ts
+++ polariis/actions/ActionGetDescription.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import { ServiceGetDescriptionMistral } from 'pxn/services/ServiceGetDescriptionMistral'
+import { ServiceTTSEleven } from 'pxn/services/ServiceTTSEleven'
 
 export const ActionGetDescription = async ({
   requirement,
@@ -10,11 +11,19 @@ export const ActionGetDescription = async ({
   requirement: string
   screenshot: string
   sourceCode: string
-}): Promise<string> => {
+}): Promise<Blob> => {
   const descriptionService = new ServiceGetDescriptionMistral()
-  return await descriptionService.describe({
-    requirement,
-    screenshot,
-    sourceCode,
+  const ttsService = new ServiceTTSEleven()
+  const { description: stringDescription } = JSON.parse(
+    await descriptionService.describe({
+      requirement,
+      screenshot,
+      sourceCode,
+    }),
+  )
+  console.log({
+    stringDescription,
   })
+  const audioDescription = await ttsService.speak(stringDescription)
+  return audioDescription
 }
---
+++ polariis/services/ServiceTTSEleven.ts
@@ -0,0 +1,28 @@
+import { ElevenLabsClient } from '@elevenlabs/elevenlabs-js'
+import { elevenClient } from './clients/eleven-client'
+
+export class ServiceTTSEleven {
+  private client: ElevenLabsClient
+  constructor() {
+    this.client = elevenClient
+  }
+
+  async speak(stringToSay: string): Promise<Blob> {
+    const audio = await this.client.textToSpeech.convert(
+      'JBFqnCBsd6RMkjVDRZzb',
+      {
+        text: stringToSay,
+        modelId: 'eleven_multilingual_v2',
+        outputFormat: 'mp3_44100_128',
+        voiceSettings: {
+          speed: 1.5,
+        },
+      },
+    )
+
+    // Ici on force à lire tout le contenu côté serveur
+    const blob = await new Response(audio).blob()
+
+    return blob
+  }
+}
